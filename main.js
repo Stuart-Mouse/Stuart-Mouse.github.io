b7c48eb4abfb319887ab97f74a8b4d30b7b073a0
Stuart_Mouse <stuart_mouse@protonmail.com>
2024-10-07 14:53:56 -0500

    test4

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\Noah\AppData\Local\Temp\tmpbcpbh_i5.js
+// include: C:\Users\Noah\AppData\Local\Temp\tmpnfao0hme.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -220,21 +220,21 @@ Module['FS_createPath']("/", "media", true, true);
 
   })();
 
-// end include: C:\Users\Noah\AppData\Local\Temp\tmpbcpbh_i5.js
-// include: C:\Users\Noah\AppData\Local\Temp\tmptejlgso0.js
+// end include: C:\Users\Noah\AppData\Local\Temp\tmpnfao0hme.js
+// include: C:\Users\Noah\AppData\Local\Temp\tmpyqkk7vru.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\Noah\AppData\Local\Temp\tmptejlgso0.js
-// include: C:\Users\Noah\AppData\Local\Temp\tmp4l0582bc.js
+  // end include: C:\Users\Noah\AppData\Local\Temp\tmpyqkk7vru.js
+// include: C:\Users\Noah\AppData\Local\Temp\tmpgde5j87z.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\Noah\AppData\Local\Temp\tmp4l0582bc.js
+  // end include: C:\Users\Noah\AppData\Local\Temp\tmpgde5j87z.js
 
 
 // Sometimes an existing Module object exists with properties

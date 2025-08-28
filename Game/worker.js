9e4088f69a38f0a3a929bbaf91b57efeec817d23
Stuart_Mouse <stuart_mouse@protonmail.com>
2025-08-27 19:48:26 -0500

    recompile game

--- Game/worker.js
+++ Game/worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "Game";
-const CACHE_TIME = "2025-08-26T04:24:47.788Z";
+const CACHE_TIME = "2025-08-27T23:44:28.494Z";
 const CACHE_FULL = CACHE_NAME + "_" + CACHE_TIME;
 const CACHED_METADATA = ["", "index.html", "icon.png", "main.wasm", "manifest.json", "runtime.js"];
 

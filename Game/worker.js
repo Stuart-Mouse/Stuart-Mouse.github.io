5e1506b6b91d4d6387ab44f1a7de464b21766dd1
Stuart_Mouse <stuart_mouse@protonmail.com>
2025-09-03 14:35:29 -0500

    renderer cleanup

--- Game/worker.js
+++ Game/worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "Game";
-const CACHE_TIME = "2025-09-03T03:42:48.709Z";
+const CACHE_TIME = "2025-09-03T19:19:10.872Z";
 const CACHE_FULL = CACHE_NAME + "_" + CACHE_TIME;
 const CACHED_METADATA = ["", "index.html", "icon.png", "main.wasm", "manifest.json", "runtime.js"];
 
